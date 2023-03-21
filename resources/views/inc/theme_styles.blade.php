dc379a173fc217dfd315950c4730666faaad0589
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-03-21 16:55:00 +0200

    add css file for coreuiv2 customizations and bs4 polyfills

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -1,5 +1,6 @@
 @basset('https://unpkg.com/@digitallyhappy/backstrap@0.5.1/dist/css/legacy.css')
 @basset('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css')
+@basset(base_path('vendor/backpack/theme-coreuiv2/resources/assets/css/coreuiv2.css'))
 
 {{-- Source Sans Font --}}
 @bassetArchive('https://github.com/adobe-fonts/source-sans/releases/download/3.046R/WOFF2-source-sans-3.046R.zip', 'source-sans-pro')
