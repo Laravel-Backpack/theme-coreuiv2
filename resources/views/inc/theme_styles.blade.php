1f743c040d4020fc1690c62111118b7218e7d26b
Mauro Martinez <maurohmartinez@gmail.com>
2023-05-19 13:32:54 +0300

    Restore font import

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -5,3 +5,10 @@
 {{-- Source Sans Font --}}
 @bassetArchive('https://github.com/adobe-fonts/source-sans/releases/download/3.046R/WOFF2-source-sans-3.046R.zip', 'source-sans-pro')
 @basset(base_path('vendor/backpack/crud/src/resources/assets/libs/source-sans-pro.css'), 'source-sans-pro/source-sans-pro.css')
+
+<link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+      rel="stylesheet">
+{{-- TODO: We should use Basset for Google Fonts too, but it doesn't quite work yet: --}}
+{{-- @basset('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap',
+true, [], 'style') --}}
