0eabaaaab50bd9eadae1004cde33ac38de63648d
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-06-28 11:32:53 +0300

    added custom attributes on anchors

--- resources/views/components/menu-dropdown.blade.php
+++ resources/views/components/menu-dropdown.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#">
+    <a {{ $attributes->merge([ 'class' => 'nav-link nav-dropdown-toggle', 'href' => $link ?? '#' ]) }}>
         @if($icon)<i class="nav-icon {{ $icon }}"></i>@endif
         @if($title) <span>{{ $title }}</span>@endif
     </a>
