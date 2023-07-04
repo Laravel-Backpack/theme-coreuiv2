eed6ac8ac9a27eb362a78c7a7497e00b57d4f27d
Pedro X <pxpm88@gmail.com>
2023-07-04 16:47:47 +0100

    add show option to dropdown

--- resources/views/components/menu-dropdown.blade.php
+++ resources/views/components/menu-dropdown.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-item nav-dropdown">
+<li class="nav-item nav-dropdown {{ $open ? 'open' : '' }}">
     <a {{ $attributes->merge([ 'class' => 'nav-link nav-dropdown-toggle', 'href' => $link ?? '#' ]) }}>
         @if($icon)<i class="nav-icon {{ $icon }}"></i>@endif
         @if($title) <span>{{ $title }}</span>@endif
