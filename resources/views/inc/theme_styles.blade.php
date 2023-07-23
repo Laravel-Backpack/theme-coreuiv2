658795b49d6171bf4cb97c759bac59ae055cfc52
Antonio Almeida <promatik@gmail.com>
2023-07-23 20:40:22 +0100

    Removed unnecessary google font load, updated source sans

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -1,14 +1,9 @@
 @basset('https://unpkg.com/@digitallyhappy/backstrap@0.5.1/dist/css/legacy.css')
 @basset('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css')
-@basset(base_path('vendor/backpack/theme-coreuiv2/resources/assets/css/coreuiv2.css'))
 
 {{-- Source Sans Font --}}
-@bassetArchive('https://github.com/adobe-fonts/source-sans/releases/download/3.046R/WOFF2-source-sans-3.046R.zip', 'source-sans-pro')
-@basset(base_path('vendor/backpack/crud/src/resources/assets/libs/source-sans-pro.css'), 'source-sans-pro/source-sans-pro.css')
+@bassetArchive('https://github.com/adobe-fonts/source-sans/releases/download/3.052R/WOFF2-source-sans-3.052R.zip', 'source-sans-pro')
+@basset(base_path('vendor/backpack/crud/src/resources/assets/libs/source-sans-pro.css'),  'source-sans-pro/source-sans-pro.css')
 
-<link rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap"
-      rel="stylesheet">
-{{-- TODO: We should use Basset for Google Fonts too, but it doesn't quite work yet: --}}
-{{-- @basset('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap',
-true, [], 'style') --}}
+{{-- Custom Backpack Rules --}}
+@basset(base_path('vendor/backpack/theme-coreuiv2/resources/assets/css/coreuiv2.css'))
