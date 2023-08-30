6b83dfff176026a4fd0623dd49c886752a13cf13
Cristian Tăbăcitu <cristian.tabacitu@digitallyhappy.com>
2023-08-30 13:17:38 +0300

    Apply suggestions from code review

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 
 <div class="row justify-content-center">
-    <div class="col-12 col-md-8 col-lg-4">
+    <div class="col-12 col-md-8 col-lg-6">
         <h3 class="text-center mb-4">{{ trans('backpack::base.verify_email.email_verification') }}</h3>
         <div class="card">
             <div class="card-body">
             {{ trans('backpack::base.verify_email.email_verification_required') }}
 
             @if (session('status') == 'verification-link-sent')
-                <div class="mb-4 mt-2">
-                    <div class="alert alert-success" role="alert">
+                <div class="mt-2">
+                    <div class="alert alert-success my-0" role="alert">
                         <div class="d-flex">
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg>
