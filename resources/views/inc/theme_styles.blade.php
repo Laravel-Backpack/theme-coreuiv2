ff0912c2c1b009a97142bc8e3f87fce166773f7f
pxpm <pxpm88@gmail.com>
2025-07-16 12:15:32 +0100

    update assets

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -1,5 +1,7 @@
-@basset('bp-noty-css')
-@basset('bp-coreui2-css')
+@basset('https://cdn.jsdelivr.net/npm/noty@3.2.0-beta-deprecated/lib/noty.min.css', true, ['integrity' => 'sha384-06UJlYrXGflQ8SjYDEf63SOUEuaWJ16nmd3nj6pttaS27WbGGJH0D1zJc0EokKhB', 'crossorigin' => 'anonymous'])
+
+{{-- CoreUI 2 CSS --}}
+@basset('https://cdn.jsdelivr.net/npm/@digitallyhappy/backstrap@0.5.1/dist/css/legacy.min.css', true, ['integrity' => 'sha384-JZChMdvSKDQzhyLHIRN9IR9eigXQIhZ4CFYpO3fKZUN20e1m8M/cvLa1c9vbaaK+', 'crossorigin' => 'anonymous'])
 
 {{-- Source Sans Font | this definition shouldn't change as the inclusion of the font in css depends on relative file paths --}}
 @bassetDirectory(base_path('vendor/backpack/theme-coreuiv2/resources/assets/fonts/source-sans-pro'), 'source-sans-pro')
